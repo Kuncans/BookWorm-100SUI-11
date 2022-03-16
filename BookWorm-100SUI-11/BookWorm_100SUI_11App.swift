bd5ad335ae6f2131ef2125bef533545f138e03cc
Kuncans <akafauna@gmail.com>
2022-03-16 23:47:48 +0000

    Initial Commit

---
+++ BookWorm-100SUI-11/BookWorm_100SUI_11App.swift
@@ -0,0 +1,17 @@
+//
+//  BookWorm_100SUI_11App.swift
+//  BookWorm-100SUI-11
+//
+//  Created by Duncan Kent on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookWorm_100SUI_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWorm-100SUI-11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookWorm-100SUI-11
+//
+//  Created by Duncan Kent on 16/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
